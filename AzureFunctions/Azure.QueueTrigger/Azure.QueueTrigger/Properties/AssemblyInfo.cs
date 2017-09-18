b1e450aa10cd73ad69b0fc1dce58d1e0f85f3475
Laurent Yin <laurent.yin@outlook.com>
2017-09-18 09:20:43 +0200

    Solution Azure.QueueTrigger added

---
+++ AzureFunctions/Azure.QueueTrigger/Azure.QueueTrigger/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Azure.QueueTrigger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Azure.QueueTrigger")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99b2e417-37a3-41ba-937d-cb50d9aa083d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzureFunctions/Azure.QueueTrigger/Azure.QueueTrigger/QueueTrigger/QueueTriggerFunction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Azure.QueueTrigger.QueueTrigger
+{
+    public class QueueTriggerFunction
+    {
+        public static async Task Run(string path, TraceWriter log)
+        {
+            string hotAzureStorageConnectionString = ConfigurationManager.AppSettings["StorageAccount.ConnectionString"];
+
+            log.Info($"Executing {typeof(QueueTriggerFunction).Name} on file {path}");
+
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(hotAzureStorageConnectionString);
+        }
+    }
+}
